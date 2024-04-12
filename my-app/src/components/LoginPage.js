85eac88677b0e7bfd331205aec2440ae227f1fb8
a-escaran <kingroller23@gmail.com>
2024-04-13 02:34:45 +1000

    base files

---
+++ my-app/src/components/LoginPage.js
@@ -0,0 +1,17 @@
+function LoginPage(){
+    return(
+        <div className="loginForm">
+            <h1>Login</h1>
+            <div className="email">
+                <h2>Username</h2>
+                <input className="emailInput"></input>
+            </div>
+            <div className="password">
+                <h2>Password</h2>
+                <input className="passInput"></input>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
